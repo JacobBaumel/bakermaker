b322e5ed5b8064fe3277a0410b9399a1f73bddce
Jacob Baumel <jacob.a.baumel@gmail.com>
2024-05-08 02:45:42 -0500

    ssh command runner

--- include/UI/ServerInstall.h
+++ include/UI/ServerInstall.h
@@ -6,11 +6,28 @@
 #define BAKERMAKER_SERVERINSTALL_H
 
 #include "UI/BaseUIScreen.h"
+#include <mutex>
+#include <thread>
+#include <atomic>
 
 namespace bakermaker {
     class ServerInstall : public BaseUIScreen {
+    private:
+        static constexpr char SEPARATOR_STR[63] = "\n\n---------------------------------------------------------\n\n\0";
+
         char e[1] = {' '};
         ST::string instructions;
+
+        ImGuiTextBuffer commandProgress;
+        std::mutex bufferMutex;
+        std::thread* exec;
+        std::atomic_int execProgress;
+        std::atomic_bool execDone;
+        bool hasStartedExec;
+        bool showCommandOutputs;
+
+        std::mutex curcmd;
+        ST::string curcmdstr;
 
     public:
         ServerInstall();
--- src/UI/ServerInstall.cpp
+++ src/UI/ServerInstall.cpp
@@ -1,9 +1,16 @@
 #include "UI/ServerInstall.h"
 #include "romfs/romfs.hpp"
 #include "setup.h"
+#include "libssh/libssh.h"
+#include "ssh_helper.h"
+
+#include "UI/InstallCommands.inc"
+
+#include <iostream>
 
 namespace bakermaker {
-    ServerInstall::ServerInstall() : BaseUIScreen(bakermaker::ProgramStage::SERVER_INSTALL) {
+    ServerInstall::ServerInstall() : BaseUIScreen(bakermaker::ProgramStage::SERVER_INSTALL),
+        execDone(false), execProgress(0), bufferMutex(), hasStartedExec(false), showCommandOutputs(false) {
         romfs::Resource md = romfs::get("ServerInstallText.md");
         instructions = ST::string((const char*) md.data(), md.size());
     }
@@ -17,6 +24,88 @@ namespace bakermaker {
         bakermaker::documentation->render(instructions);
 
         ImGui::NewLine();
-        ImGui::Button("Begin");
+        if(ImGui::Button("Begin##server_install_begin")) {
+            hasStartedExec = true;
+
+            exec = new std::thread([this](){
+                ssh_session ses;
+                memset((void*) &ses, 0, sizeof(ssh_session));
+                bakermaker::createSession(ses, std::string(config["server"]["ip"]).c_str(),
+                                          std::string(config["server"]["user"]).c_str(),
+                                          std::string(config["server"]["keyfile"]).c_str(), 0);
+
+                ssh_channel ch;
+                char buffer[256];
+                int nbytes = 0;
+
+                for(const auto& cmd : bakermaker::commands) {
+                    curcmd.lock();
+                    curcmdstr = cmd;
+                    curcmd.unlock();
+
+                    bufferMutex.lock();
+                    commandProgress.append(SEPARATOR_STR);
+                    bufferMutex.unlock();
+
+                    ch = ssh_channel_new(ses);
+                    ssh_channel_open_session(ch);
+                    ssh_channel_request_exec(ch, cmd);
+
+                    nbytes = ssh_channel_read(ch, buffer, sizeof(buffer), 0);
+                    while(nbytes > 0) {
+                        bufferMutex.lock();
+                        commandProgress.append(buffer, buffer + nbytes);
+                        bufferMutex.unlock();
+                        nbytes = ssh_channel_read(ch, buffer, sizeof(buffer), 0);
+                    }
+
+                    ssh_channel_send_eof(ch);
+                    ssh_channel_close(ch);
+                    ssh_channel_free(ch);
+                }
+
+                curcmd.lock();
+                curcmdstr = "Finished";
+                curcmd.unlock();
+
+                bufferMutex.lock();
+                commandProgress.append(SEPARATOR_STR);
+                bufferMutex.unlock();
+
+                ssh_disconnect(ses);
+                ssh_free(ses);
+            });
+        }
+
+        if(hasStartedExec) {
+            if(ImGui::Button("Show/Hide Command Outputs##serverinstall_commandoutputs")) {
+                showCommandOutputs = !showCommandOutputs;
+            }
+
+            if(showCommandOutputs) {
+                ImGui::SetNextWindowSize(ImVec2(500, 500), ImGuiCond_FirstUseEver);
+                if(ImGui::Begin("Command Output")) {
+                    ImGui::PushFont(fontlist[1]);
+                    curcmd.lock();
+                    ImGui::Text("Current Command: %s", curcmdstr.c_str());
+                    curcmd.unlock();
+                    ImGui::PopFont();
+
+                    ImGui::Separator();
+
+                    if(ImGui::BeginChild("cmdout##serverinstall")) {
+                        bufferMutex.lock();
+                        ImGui::TextUnformatted(commandProgress.c_str());
+                        bufferMutex.unlock();
+                    }
+
+                    ImGui::EndChild();
+                }
+
+                ImGui::End();
+            }
+        }
+
+
     }
 }
